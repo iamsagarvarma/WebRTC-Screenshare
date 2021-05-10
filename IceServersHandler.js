2b60484d49b000886d47cc93bb2e0bf87231954d
iamsagarvarma <32607188+iamsagarvarma@users.noreply.github.com>
2021-05-10 12:07:39 +0530

    Update IceServersHandler.js
    added custom turn server

--- IceServersHandler.js
+++ IceServersHandler.js
@@ -4,7 +4,16 @@ var IceServersHandler = (function() {
     function getIceServers(connection) {
         // resiprocate: 3344+4433
         // pions: 7575
-        var iceServers = [];
+        var iceServers = [
+         {
+                'urls': [
+                    'turn:14.141.53.98:3478',
+                    'turn:14.141.53.98:3478?transport=tcp'
+                ],
+                'username': 'test',
+                'credential': 'test1234#%'
+            }
+        ];
 
         return iceServers;
     }
